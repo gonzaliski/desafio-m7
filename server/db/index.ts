746161d0154edba26829e8731e5a889847fc8cb7
Gon <gon.mendez13@gmail.com>
2022-08-17 15:39:47 -0300

    change in db conn

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,11 +1,11 @@
 import { Sequelize} from "sequelize"
 export const sequelize = new Sequelize({
     dialect: "postgres",
-    username: "dpdojpwzgwopmc",
-    password: "c714e61a9f8e3d9f5ffd4ee2b176642b08a631d79060ea16f92f1fb9b3c61871",
-    database: "d3q7ilpsc7n0rc",
+    username: process.env.SEQUELIZE_USERNAME,
+    password:process.env.SEQUELIZE_PASSWORD,
+    database: process.env.SEQUELIZE_DATABASE,
     port: 5432,
-    host: "ec2-44-198-82-71.compute-1.amazonaws.com",
+    host: process.env.SEQUELIZE_host,
     ssl: true,
     // esto es necesario para que corra correctamente
     dialectOptions: {
