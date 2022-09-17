60f0e4995902fbe141825abd1fc39c3ef39c1061
Gon <gon.mendez13@gmail.com>
2022-09-17 13:18:43 -0300

    fixed sendgrid email title

--- server/controllers/reports-controller.ts
+++ server/controllers/reports-controller.ts
@@ -21,7 +21,7 @@ export async function createReport(petId, data){
         const msg = {
         to: `${userFromPet.get("email")}`,
         from: 'petfinder.apx@gmail.com', // Use the email address or domain you verified above
-        subject: `Se ha reportado informacion de ${data.petName}!`,
+        subject: `Se ha reportado informacion de ${pet.get("name")}!`,
         text: `
         De:${data.reporterName}
         Telefono:${data.phoneNumber}
